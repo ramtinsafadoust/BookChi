381f0f081c4d8ec94819f9a68e527dc5d3aff36a
Ramtin Safadoust <ramtin.safadoust@gmail.com>
2020-03-18 21:05:13 +0330

    Search Is Done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from config import development
 import sqlite3
 
@@ -8,11 +8,23 @@ conn=sqlite3.connect('database.sqlite')
 cur = conn.cursor()
 cur.execute("SELECT * FROM product")
 rows = cur.fetchall()
-for row in rows:
-        print(row)
+
+
+
 
 
 @app.route("/")
 def index():
-   
     return render_template("index.html",data=rows)
+
+
+@app.route("/search",methods=["get"])
+def search():
+    data=   request.args.get("search")
+    print(data)
+    conn=sqlite3.connect('database.sqlite')
+    cur = conn.cursor()
+    qur="SELECT * FROM product WHERE id LIKE "+"'%"+data+"%' OR bookname LIKE "+"'%"+data+"%'OR writer LIKE "+"'%"+data+"%' OR gorooh LIKE "+"'%"+data+"%' OR boxindex LIKE "+"'%"+data+"%'OR barcode LIKE "+"'%"+data+"%'"
+    cur.execute(qur)
+    searchrows=cur.fetchall()
+    return render_template("search.html",data=searchrows)
