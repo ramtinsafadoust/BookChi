faa919aa148b4948022089deb8bfb96f03207dcf
ramtin safadoust <ramtin.safadoust@gmail.com>
2020-03-21 18:56:58 +0430

    Search Is Ok Now

--- app.py
+++ app.py
@@ -145,21 +145,6 @@ def load_user(id,username,password):
     return User(id,username,password)
     
 
-
-@app.route("/search",methods=["get"])
-def search():
-    data=   request.args.get("search")
-    print(data)
-    conn=sqlite3.connect('database.sqlite')
-    cur = conn.cursor()
-    qur="SELECT * FROM product WHERE id LIKE "+"'%"+data+"%' OR bookname LIKE "+"'%"+data+"%'OR writer LIKE "+"'%"+data+"%' OR gorooh LIKE "+"'%"+data+"%' OR boxindex LIKE "+"'%"+data+"%'OR barcode LIKE "+"'%"+data+"%'"
-    cur.execute(qur)
-    searchrows=cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template("search.html",data=searchrows)
-
-
 @app.route("/product",methods=["GET","POST"])
 def product():
     bookname=request.form.get("bookname")
@@ -219,6 +204,24 @@ def update():
     cur.close()
     conn.close()
     return rows
+
+
+
+
+
+@app.route("/search",methods=["get"])
+def search():
+    data=   request.args.get("search")
+    print(data)
+    conn=sqlite3.connect('database.sqlite')
+    cur = conn.cursor()
+    qur="SELECT * FROM product WHERE id LIKE "+"'%"+data+"%' OR bookname LIKE "+"'%"+data+"%'OR writer LIKE "+"'%"+data+"%' OR gorooh LIKE "+"'%"+data+"%' OR boxindex LIKE "+"'%"+data+"%'OR barcode LIKE "+"'%"+data+"%'"
+    cur.execute(qur)
+    searchrows=cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("search.html",data=searchrows)
+
 
 
 
